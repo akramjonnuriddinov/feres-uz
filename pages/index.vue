1cdb8a21ec3208542665aaee69d13b61f8269679
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-04-30 08:55:26 +0500

    Add all news button to home page

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,13 @@
     <div class="py-12 v-container">
       <h2 class="pb-12 mb-2 text-center title">Our latest news</h2>
       <news-card />
+      <div class="flex justify-center">
+        <nuxt-link
+          to="/news"
+          class="mx-1 uppercase border border-brown text-[#373737] rounded-md transition-all font-medium duration-200 px-4 py-2 hover:bg-brown hover:text-white"
+          >All</nuxt-link
+        >
+      </div>
     </div>
   </section>
 </template>
