6ee269838bfaf29e557e005b27c7f3ca05d208b8
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-05-01 15:12:28 +0500

    Add news-card to news page

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>News main page</div>
+  <section class="bg-[#f9fafb]">
+    <div class="py-12 v-container">
+      <h2 class="pb-12 mb-2 text-center title">Our latest news</h2>
+      <news-card />
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts"></script>
