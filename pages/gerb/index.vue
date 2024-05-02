8f2a527e943884f3585f685c54ae917815eebf76
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-05-02 16:16:04 +0500

    Remove don't need code

--- pages/gerb/index.vue
+++ pages/gerb/index.vue
@@ -14,39 +14,3 @@ definePageMeta({
     <app-socials />
   </div>
 </template>
-
-<style scoped>
-.social-link {
-  position: relative;
-  display: inline-block;
-  transition: all 0.4s linear;
-}
-
-.social-link::before,
-.social-link::after {
-  content: '';
-  position: absolute;
-  box-sizing: border-box;
-  width: 100%;
-  height: 100%;
-  left: 0;
-  top: 0;
-  transition: 0.4s linear;
-}
-
-.social-link:hover {
-  transform: scale(0.7);
-}
-
-.social-link:hover::before {
-  border-left: 2px solid;
-  border-right: 2px solid;
-  transform: skewX(20deg);
-}
-
-.social-link:hover::after {
-  border-top: 2px solid;
-  border-bottom: 2px solid;
-  transform: skewY(-20deg);
-}
-</style>
