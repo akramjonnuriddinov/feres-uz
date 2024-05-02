9cd44f5393b045251c783e164766886c6d28e7df
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-05-02 19:20:07 +0500

    Change some styles of AppAccessibility

--- components/AppAccessibility.vue
+++ components/AppAccessibility.vue
@@ -13,7 +13,7 @@ const buttons = ref([
     className: 'invert text-white',
   },
   {
-    name: 'no-image',
+    name: 'no_image',
     className: 'no-image text-black',
   },
 ])
@@ -21,15 +21,15 @@ const buttons = ref([
 const handleClick = (id: any) => {
   if (id == 'grayscale') document.body.classList.add('grayscale')
   if (id == 'invert') document.body.classList.add('invert')
-  if (id == 'no-image') document.body.classList.add('no-image')
+  if (id == 'no_image') document.body.classList.add('no_image')
   if (id == 'normal') document.body.className = ''
 }
 </script>
 
 <template>
   <div class="bg-white border w-[267px] h-[182px] p-2">
-    <h3>Ko'rinish</h3>
-    <div class="flex items-start gap-1">
+    <h3 class="text-[#343a40] mb-2 font-medium">Ko'rinish</h3>
+    <div class="flex items-start gap-1 mb-4">
       <button
         v-for="button in buttons"
         @click="handleClick(button.name)"
@@ -41,7 +41,7 @@ const handleClick = (id: any) => {
         <icon-image class="w-6 h-6" />
       </button>
     </div>
-    <h3>Shrift o'lchami</h3>
+    <h3 class="text-[#343a40] mb-2 font-medium">Shrift o'lchami</h3>
   </div>
 </template>
 
@@ -52,7 +52,7 @@ body.grayscale {
 body.invert {
   filter: invert(100%) grayscale(100%);
 }
-body.no-image img {
+body.no_image img {
   visibility: hidden;
 }
 .normal {
