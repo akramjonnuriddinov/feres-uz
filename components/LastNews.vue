a5f49081ac7ac13880c43a984c92ad9dfc7c42b3
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-05-02 07:50:51 +0500

    Add LastNews component

---
+++ components/LastNews.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="md:max-w-[33%] w-full">
+    <h3 class="flex items-center font-medium text-xl mb-[22px]">
+      <span
+        class="block w-5 h-5 mr-2 border-2 rounded-full border-brown"
+      ></span>
+      <span>Сўнгги янгиликлар</span>
+    </h3>
+    <ul>
+      <li class="px-4 py-1 my-2 rounded border border-[#6c757d]" v-for="i in 5">
+        <div class="flex items-center mb-4 text-base">
+          <icon-calendar class="w-4 h-4 mr-1" />
+          <span>24:11:2022</span>
+        </div>
+        <h4 class="mb-2 overflow-hidden text-xl font-medium line-clamp-4">
+          <nuxt-link to="/">
+            “FARG‘ONA HETK” AJ TOMONIDAN MUQOBIL ENERGIYA MANBALARINI JORIY
+            ETISHGA ALOHIDA E’TIBOR AJ TOMONIDAN MUQOBIL ENERGIYA MANBALARINI
+            JORIY ETISHGA ALOHIDA E’TIBOR AJ TOMONIDAN MUQOBIL ENERGIYA
+          </nuxt-link>
+        </h4>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/info/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="py-[30px]">
+    <div class="v-container">
+      <div class="flex flex-col md:flex-row">
+        <div class="mb-[30px] w-full md:mr-[30px]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          reiciendis fugit facere omnis assumenda quas accusantium quisquam
+          optio! Laboriosam sequi reiciendis iure ullam facere explicabo atque
+          illo qui, mollitia cum.
+        </div>
+        <last-news />
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
